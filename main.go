bd54e53aa1eba560b579f863eb6c563e570bd38c
benst <bensteier713@gmail.com>
2024-11-03 19:31:44 -0600

    added go.mod and initial 'web app'

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
-	fmt.Print("hello world")
+	//fmt.Print("hello world")
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		n, err := fmt.Fprintf(w, "hello world")
+		if err != nil {
+			fmt.Printf("error found %s", err.Error())
+		}
+
+		fmt.Printf("bytes written: %d", n)
+	})
+
+	_ = http.ListenAndServe(":8080", nil)
 }
