cc184f8b141a46ba60fe3e76d36185bb4ad8ebfe
benst <bensteier713@gmail.com>
2024-11-03 20:46:21 -0600

    added template pages and updated rendering template

--- main.go
+++ main.go
@@ -2,20 +2,36 @@ package main
 
 import (
 	"fmt"
+	"html/template"
 	"net/http"
 )
+
+const portNumber = ":8080"
+
+// All Handler functions require TWO arguements. A ResponseWriter and a Request
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
+func renderTemplate(w http.ResponseWriter, tmpl string) {
+	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
+	err := parsedTemplate.Execute(w, nil)
+	if err != nil {
+		fmt.Println("error parsing template: ", err)
+		return
+	}
+}
 
 func main() {
 	//fmt.Print("hello world")
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		n, err := fmt.Fprintf(w, "hello world")
-		if err != nil {
-			fmt.Printf("error found %s", err.Error())
-		}
+	http.HandleFunc("/", Home)
+	http.HandleFunc("/about", About)
 
-		fmt.Printf("bytes written: %d", n)
-	})
-
-	_ = http.ListenAndServe(":8080", nil)
+	_ = http.ListenAndServe(portNumber, nil)
 }
