dfe22694e07bc2760a133a383fb92d2a216d80f0
benst <bensteier713@gmail.com>
2024-11-03 21:00:51 -0600

    same as previous comit

---
+++ handlers.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"net/http"
+)
+
+// All Handler functions require TWO arguements. A ResponseWriter and a Request
+func Home(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "home.page.tmpl")
+}
+
+// About is the about page handler
+func About(w http.ResponseWriter, r *http.Request) {
+	renderTemplate(w, "about.page.tmpl")
+}
+
