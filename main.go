600e18f301f5fb6314e976406c161e50d95e11c7
benst <bensteier713@gmail.com>
2024-11-03 20:58:55 -0600

    added bootstrap css to templates. Added templates. Added render/handlers go files to separate out main file to be less cluttered

--- main.go
+++ main.go
@@ -1,31 +1,10 @@
 package main
 
 import (
-	"fmt"
-	"html/template"
 	"net/http"
 )
 
 const portNumber = ":8080"
-
-// All Handler functions require TWO arguements. A ResponseWriter and a Request
-func Home(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "home.page.tmpl")
-}
-
-// About is the about page handler
-func About(w http.ResponseWriter, r *http.Request) {
-	renderTemplate(w, "about.page.tmpl")
-}
-
-func renderTemplate(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/" + tmpl)
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
-		return
-	}
-}
 
 func main() {
 	//fmt.Print("hello world")
