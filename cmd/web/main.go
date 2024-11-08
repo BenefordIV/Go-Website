89bd0b8cbcb4a26bc5751efdf709b21efef3e804
benst <bensteier713@gmail.com>
2024-11-07 21:34:39 -0600

    using config file and template caching

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,18 +1,33 @@
 package main
 
 import (
+	"log"
 	"net/http"
 
+	"github.com/Beneford_IV/udemy_app/pkg/config"
 	"github.com/Beneford_IV/udemy_app/pkg/handlers"
+	ren "github.com/Beneford_IV/udemy_app/pkg/render"
 )
 
 const portNumber = ":8080"
 
 func main() {
-	//fmt.Print("hello world")
+	var app config.AppConfig
+	tc, err := ren.CreateTemplateCache()
+	if err != nil {
+		log.Fatal("Cannot create template cache")
+	}
 
-	http.HandleFunc("/", handlers.Home)
-	http.HandleFunc("/about", handlers.About)
+	app.TemplateCache = tc
+
+	app.UseCache = false
+
+	repo := handlers.NewRepo(&app)
+	handlers.NewHandlers(repo)
+
+	ren.NewTemplates(&app)
+	http.HandleFunc("/", handlers.Repo.Home)
+	http.HandleFunc("/about", handlers.Repo.About)
 
 	_ = http.ListenAndServe(portNumber, nil)
 }
--- pkg/render/render.go
+++ pkg/render/render.go
@@ -1,59 +1,89 @@
 package render
 
 import (
-	"fmt"
+	"bytes"
 	"html/template"
 	"log"
 	"net/http"
+	"path/filepath"
+
+	"github.com/Beneford_IV/udemy_app/pkg/config"
+	"github.com/Beneford_IV/udemy_app/pkg/models"
 )
 
-func RenderTemplateTest(w http.ResponseWriter, tmpl string) {
-	parsedTemplate, _ := template.ParseFiles("./templates/"+tmpl, "./templates/base.layout.gohtml")
-	err := parsedTemplate.Execute(w, nil)
-	if err != nil {
-		fmt.Println("error parsing template: ", err)
+var funjctinMap = template.FuncMap{}
+var app *config.AppConfig
+
+// Constructor for new Template Package
+func NewTemplates(a *config.AppConfig) {
+	app = a
+}
+
+func AddDefaultData(td *models.TemplateData) *models.TemplateData {
+	return td
+}
+
+func RenderTemplate(w http.ResponseWriter, tmpl string, td *models.TemplateData) {
+	var tc map[string]*template.Template
+	if app.UseCache {
+		tc = app.TemplateCache
+	} else {
+		tc, _ = CreateTemplateCache()
+	}
+
+	//get requested template from cache
+	t, ok := tc[tmpl]
+	if !ok {
+		log.Fatal("error template not in cache: ")
 		return
 	}
+
+	buf := new(bytes.Buffer)
+	_ = t.Execute(buf, td)
+
+	//render template
+	_, err := buf.WriteTo(w)
+	if err != nil {
+		log.Println(err)
+	}
 }
 
-var tc = make(map[string]*template.Template)
+func CreateTemplateCache() (map[string]*template.Template, error) {
+	cache := map[string]*template.Template{}
 
-func RenderTemplate(w http.ResponseWriter, t string) {
-	var tmpl *template.Template
-	var err error
+	//get all files named *.page.gohtml
+	pages, err := filepath.Glob("./templates/*.page.gohtml")
+	if err != nil {
+		log.Println("error parsing template: ", err)
+		return cache, err
+	}
 
-	//check to see if we already ahve the template in our cache
-	_, inmap := tc[t]
-	if !inmap {
-		err = createTemplateCache(t)
+	//loop through all pages
+	for _, page := range pages {
+		name := filepath.Base(page)
+		log.Println("Page is currently ", page)
+
+		ts, err := template.New(name).ParseFiles(page)
 		if err != nil {
-			log.Println("Error creating template cache", err)
+			log.Println("error parsing template: ", err)
+			return cache, err
 		}
-	} else {
-		log.Println("Template already in cache")
+
+		matches, err := filepath.Glob("./templates/*.layout.gohtml")
+		if err != nil {
+			log.Println("error parsing template: ", err)
+			return cache, err
+		}
+
+		if len(matches) > 0 {
+			ts, err = ts.ParseGlob("./templates/*.layout.gohtml")
+			if err != nil {
+				log.Println("error parsing template: ", err)
+				return cache, err
+			}
+		}
+		//add page to cache
+		cache[name] = ts
 	}
-
-	tmpl = tc[t]
-
-	err = tmpl.Execute(w, nil)
-	if err != nil {
-		log.Println("Error creating template cache", err)
-	}
-}
-
-func createTemplateCache(t string) error {
-	templates := []string{
-		fmt.Sprintf("./templates/%s", t),
-		"./templates/base.layout.gohtml",
-	}
-
-	//parse template
-	tmpl, err := template.ParseFiles(templates...)
-	if err != nil {
-		return err
-	}
-
-	//add template to cache
-	tc[t] = tmpl
-	return nil
+	return cache, nil
 }
